9d3480c3263f1e8a1a0d7a7fbe3c7db59de4c462
-Corgi- <99050972+Cor-gi@users.noreply.github.com>
2022-09-08 17:31:48 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById('Canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Boundary {
+    constructor({ position }) {
+        this.position = position
+        this.width = 40
+        this.height = 40
+    }
+
+    draw() {
+        c.fillStyle = 'rgb(14, 106, 131)'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+
+mapped.forEach((array, n) => {
+    array.forEach((row, i) => {
+        row.forEach((symbol, j) => {
+            switch (symbol) {
+                case '-':
+                    boundaries.push(new Boundary({
+                        position: {
+                            x: 40 * j + (150*n),
+                            y: 40 * i
+                        }
+                    }))
+                    console.log(n)
+                    break;
+            }
+        })
+    })
+})
+
+
+boundaries.forEach((boundary) => {
+    boundary.draw()
+})
---
+++ mapper.js
@@ -0,0 +1,24 @@
+let textin = 'abcd'
+let textout = [...textin]
+
+const mapped = []
+
+textout.forEach((letter => {
+    switch (letter) {
+        case 'a':
+            mapped.push(la)
+            break;
+        case 'b':
+            mapped.push(lb)
+            break;
+        case 'c':
+            mapped.push(lc)
+            break;
+        case 'd':
+            mapped.push(ld)
+            break;
+        
+    }
+}))
+
+console.log(mapped)
